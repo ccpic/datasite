18060ec2bb0b4cf022f6c3a50bf055bab9d0b5a5
ccpic <13426257846@163.com>
2021-01-12 12:21:11 +0800

    Continue the construction of Internal_sales app

--- internal_sales/views.py
+++ internal_sales/views.py
@@ -36,29 +36,50 @@ def index(request):
 
 
 def query(request):
-    print(request)
     form_dict = dict(six.iterlists(request.GET))
-    pivoted = get_df(form_dict)
+    df = get_df(form_dict)
 
-    sql = "Select * From %s Where TAG = '%s' AND CITY = '%s'" % (DB_TABLE, "销量", "合肥市")
-    df = pd.read_sql_query(sql, ENGINE)
+    table = get_ptable_monthly(df)
+    ptable_monthly = table.to_html(
+        escape=False,
+        formatters=build_formatters_by_col(table),  # 逐列调整表格内数字格式
+        classes="ui selectable celled table",  # 指定表格css class为Semantic UI主题
+        table_id="ptable_monthly",  # 指定表格id
+    )
 
-    context = {"ptable": pivoted.to_html()}
-
-    print(pivoted.to_html())
+    context = {"ptable_monthly": ptable_monthly}
 
     return HttpResponse(
         json.dumps(context, ensure_ascii=False), content_type="application/json charset=utf-8",
     )  # 返回结果必须是json格式
 
 
-def get_df(form_dict, is_pivoted=True):
+def get_ptable_monthly(df):
+    df.fillna(0, inplace=True)
+    df_ptable = df[df.index >= 202001].T
+    # df_ptable["趋势"] = 1
+    # df_ptable["趋势"] = df_ptable["趋势"].apply(
+    #     lambda x: (
+    #         '<span id="trend_sparkline" class="inlinesparkline">1,2,3,4,5</span>'
+    #         # + df_ptable.apply(lambda row: ",".join(row.dropna().astype(str).tolist()), axis=1)
+    #     )
+    #     # .to_string(header=False, index=False)
+    #     .format(x)
+    # )
+
+    return df_ptable
+
+
+def get_df(form_dict, tag="销量", is_pivoted=True):
     sql = sqlparse(form_dict)  # sql拼接
     print(sql)
     df = pd.read_sql_query(sql, ENGINE)  # 将sql语句结果读取至Pandas Dataframe
 
+    df = df[df.TAG == tag]  # 区分销售、药房销售和指标
+
     if is_pivoted is True:
-        dimension_selected = form_dict["DIMENSION_select"][0]
+        dimension_selected = form_dict["DIMENSION_select"][0]  # 分析维度
+        unit_selected = form_dict["UNIT_select"][0]  # 单位（盒数、标准盒数、金额）
         if dimension_selected[0] == "[":
 
             column = dimension_selected[1:][:-1]
@@ -67,13 +88,16 @@ def get_df(form_dict, is_pivoted=True):
 
         pivoted = pd.pivot_table(
             df,
-            values="AMOUNT",  # 数据透视汇总值为AMOUNT字段，一般保持不变
-            index="DATE",  # 数据透视行为DATE字段，一般保持不变
+            values=unit_selected,  # 数据透视汇总值为AMOUNT字段，一般保持不变
+            index="FILL_DATE",  # 数据透视行为DATE字段，一般保持不变
             columns=column,  # 数据透视列为前端选择的分析维度
             aggfunc=np.sum,
         )  # 数据透视汇总方式为求和，一般保持不变
         if pivoted.empty is False:
             pivoted.sort_values(by=pivoted.index[-1], axis=1, ascending=False, inplace=True)  # 结果按照最后一个DATE表现排序
+
+        pivoted = pd.DataFrame(pivoted.to_records())
+        pivoted.set_index("FILL_DATE", inplace=True)
 
         return pivoted
     else:
@@ -82,11 +106,7 @@ def get_df(form_dict, is_pivoted=True):
 
 def sqlparse(context):
     print(context)
-    sql = "Select * from %s Where PERIOD = '%s' And UNIT = '%s'" % (
-        DB_TABLE,
-        context["PERIOD_select"][0],
-        context["UNIT_select"][0],
-    )  # 先处理单选部分
+    sql = "Select * from %s WHERE 1=1" % DB_TABLE  # 先处理单选部分
     print(context["customized_sql"])
     if context["customized_sql"][0] == "":
         # 如果前端没有输入自定义sql，直接循环处理多选部分进行sql拼接
@@ -153,3 +173,24 @@ def search(request, column, kw):
     return HttpResponse(
         json.dumps(res, ensure_ascii=False), content_type="application/json charset=utf-8",
     )  # 返回结果必须是json格式
+
+
+def build_formatters_by_col(df):
+    format_abs = lambda x: "{:,.0f}".format(x)
+    format_share = lambda x: "{:.1%}".format(x)
+    format_gr = lambda x: "{:.1%}".format(x)
+    format_currency = lambda x: "¥{:,.1f}".format(x)
+    d = {}
+    for column in df.columns:
+        if "份额" in str(column) or "贡献" in str(column):
+            d[column] = format_share
+        elif "价格" in str(column) or "单价" in str(column):
+            d[column] = format_currency
+        elif "同比增长" in str(column) or "增长率" in str(column) or "CAGR" in str(column) or "同比变化" in str(column):
+            d[column] = format_gr
+        elif "趋势" in str(column):
+            d[column] = None
+        else:
+            d[column] = format_abs
+
+    return d
