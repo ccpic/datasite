49f526e4c28299acae26fc6e0497423b958d00a6
ccpic <13426257846@163.com>
2021-01-21 12:52:25 +0800

    Continue the construction of Internal_sales app

--- internal_sales/views.py
+++ internal_sales/views.py
@@ -11,7 +11,7 @@ from chpa_data.charts import *
 
 ENGINE = create_engine("mssql+pymssql://(local)/Internal_sales")  # 创建数据库连接引擎
 DB_TABLE = "data"
-date = datetime.datetime(year=2020, month=11, day=1)
+date = datetime.datetime(year=2020, month=12, day=1)
 date_ya = date.replace(year=date.year - 1)  # 同比月份
 date_year_begin = date.replace(month=1)  # 本年度开头
 date_ya_begin = date_ya.replace(month=1)  # 去年开头
